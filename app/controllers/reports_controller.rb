8695101713ef264e9d9a7a5d13d0a39e1b04cad9
Ashish <ash.ssj@gmail.com>
2015-11-25 11:59:51 +0530

    testing report fixes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,14 +19,31 @@ class ReportsController < ApplicationController
 
 				@billed = params[:is_billed].sub! '?reload', ''
 				@is_billed = @billed
-				@timesheet = Timesheet.select("client_id, project_id, sum(hours) AS total_hours, EXTRACT(Month from timesheetdate) as month,
+
+				if params[:report_type] == 'monthly'
+
+					@timesheet = Timesheet.sselect("client_id, project_id, sum(hours) AS total_hours, EXTRACT(Month from timesheetdate) as month,role_id, rate, is_billed, employee_id").group("client_id, project_id, month, role_id, rate, is_billed, employee_id").where("timesheetdate >= ? AND timesheetdate <= ? AND is_billed = ?", params[:from_date], params[:to_date], @is_billed ).order("client_id,project_id")
+
+
+				else
+					@timesheet = Timesheet.select("client_id, project_id, sum(hours) AS total_hours, EXTRACT(Month from timesheetdate) as month,
 				(extract(week from timesheetdate)) as week,
 				 role_id, rate, is_billed, employee_id").group("client_id, project_id, month, week, role_id, rate, is_billed, employee_id").where("timesheetdate >= ? AND timesheetdate <= ? AND is_billed = ?", params[:from_date], params[:to_date], @is_billed ).order("client_id,project_id")
+				end
+
 
 			else
-				@timesheet = Timesheet.select("client_id, project_id, sum(hours) AS total_hours, EXTRACT(Month from timesheetdate) as month,
+
+				if params[:report_type] == 'monthly'
+
+					@timesheet = Timesheet.select("client_id, project_id, sum(hours) AS total_hours, EXTRACT(Month from timesheetdate) as month,role_id, rate, is_billed, employee_id").group("client_id, project_id, month, role_id, rate, is_billed, employee_id").where("timesheetdate >= ? AND timesheetdate <= ?", params[:from_date], params[:to_date] ).order("client_id,project_id")
+
+				else
+
+					@timesheet = Timesheet.select("client_id, project_id, sum(hours) AS total_hours, EXTRACT(Month from timesheetdate) as month,
 				(extract(week from timesheetdate)) as week,
 				 role_id, rate, is_billed, employee_id").group("client_id, project_id, month, week, role_id, rate, is_billed, employee_id").where("timesheetdate >= ? AND timesheetdate <= ?", params[:from_date], params[:to_date] ).order("client_id,project_id")
+				end
 			end
 
 		else
